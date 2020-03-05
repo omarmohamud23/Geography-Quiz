3b198ac822ed157b002b2992e37738106c32120e
Omar Mohamud <qx2316io@go.minneapolis.edu>
2020-03-05 13:27:52 -0600

    Add project files.

---
+++ Geography Quiz/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Geography_Quiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCheckAnswer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Which ocean is the largest";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(0, 54);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 20);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // btnCheckAnswer
+            // 
+            this.btnCheckAnswer.Location = new System.Drawing.Point(196, 51);
+            this.btnCheckAnswer.Name = "btnCheckAnswer";
+            this.btnCheckAnswer.Size = new System.Drawing.Size(75, 23);
+            this.btnCheckAnswer.TabIndex = 2;
+            this.btnCheckAnswer.Text = "check";
+            this.btnCheckAnswer.UseVisualStyleBackColor = true;
+            this.btnCheckAnswer.Click += new System.EventHandler(this.btnCheckAnswer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(388, 101);
+            this.Controls.Add(this.btnCheckAnswer);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCheckAnswer;
+    }
+}
+
---
+++ Geography Quiz/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geography_Quiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheckAnswer_Click(object sender, EventArgs e)
+        {
+            string answer = txtAnswer.Text;
+            answer = answer.ToLower();
+         
+            
+            if(answer.Contains("pacific"))
+            {
+                MessageBox.Show("Correct!", "Result");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect, the asnwer is the Pacific Ocean", "Result");
+            }
+        }
+    }
+}
